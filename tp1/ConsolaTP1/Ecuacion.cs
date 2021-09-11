63fc02cc6e9c7b79cb79fe102b01db909e2b9377
LeanProg <leomasterx10@gmail.com>
2021-09-11 15:55:17 -0300

    Punto 1 y 2 TP1

---
+++ tp1/ConsolaTP1/Ecuacion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaTP1
+{
+    class Ecuacion
+    {
+        private int numerouno;
+        private int numerodos;
+
+        public Ecuacion(int numerouno, int numerodos)
+        {
+            this.Numerouno = numerouno;
+            this.Numerodos = numerodos;
+        }
+
+        public int Numerouno { get => numerouno; set => numerouno = value; }
+        public int Numerodos { get => numerodos; set => numerodos = value; }
+
+
+        public static int Suma(Ecuacion miEcuacion)
+        {
+            int suma = miEcuacion.numerouno + miEcuacion.Numerodos;
+
+            return suma;
+        }
+        public static int Division(Ecuacion miEcuacion)
+        {
+            int division;
+            try
+            {
+                division = miEcuacion.numerouno / miEcuacion.numerodos;
+             
+            }
+            catch(Exception ex)
+            {
+                string error = ex.Message;
+                Console.Write("Error: "+ error);
+                return 002;
+            }
+            return division;
+        }
+        public static void Cuadrado(Ecuacion numeros)
+        {
+            int potencia2 = numeros.Numerodos * numeros.Numerodos;
+            Console.WriteLine("Cuadrado del numero 2: "+ potencia2);
+            potencia2 = numeros.numerouno * numeros.numerouno;
+            Console.WriteLine("Cuadrado del numero 1: " + potencia2);
+
+        }
+    }
+}
---
+++ tp1/ConsolaTP1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsolaTP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int numero1, numero2;
+            Console.WriteLine("Ingrese dos numeros para sumar y dividir ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            Ecuacion misNumeros = new Ecuacion(numero1,numero2);
+            int suma = Ecuacion.Suma(misNumeros);
+            int division = Ecuacion.Division(misNumeros);
+            Console.WriteLine("Resultado de la suman: "+suma);
+            if (division == 002) { Console.WriteLine("Nose puede dividir por cero intentelo de nuevo"); }
+            else { Console.WriteLine("resultado de la division: " + division); }
+            Ecuacion.Cuadrado(misNumeros);
+
+        }
+    }
+}
